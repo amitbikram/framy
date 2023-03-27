7663c38e134b26c6b86ce971a5ab348fb835a475
Amit Bikram Sarangi <sarangi@Amits-MacBook-Pro-2.local>
2023-03-28 01:08:45 +0530

    code setup

---
+++ Readme.md
@@ -0,0 +1,12 @@
+
+Runs on rust nightly
+
+Below command to be run:
+
+`rustup install nightly`
+
+`rustup default nightly`
+
+`trunk serve`
+
+`bacon`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use wasm_bindgen;
+use web_sys::{self, window};
+use leptos_reactive;
+
 fn main() {
-    println!("Hello, world!");
+    let window = window().unwrap();
+    let document = window.document().unwrap();
+    let body = document.body().unwrap();
+
+    let h1 = document.create_element("h1").unwrap();
+    h1.set_text_content(Some("Hello WASM!"));
+    body.append_child(&h1).unwrap();
 }
